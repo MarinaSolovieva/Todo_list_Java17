516aa1b110fc64d78f310033851d1b68a27eafd3
MarinaSolovieva <marishasolovieva@mail.ru>
2022-01-13 13:36:53 +0300

    refactoring

--- src/test/resources/fill-users-table-before.sql
+++ src/test/resources/fill-users-table-before.sql
@@ -4,3 +4,4 @@ insert into users (id, name, surname) values (1, 'Marina', 'Solovieva');
 insert into users (id, name, surname) values (2, 'Artsiom', 'Barkhatau');
 insert into users (id, name, surname) values (3, 'Diana', 'Gamaunova');
 
+
